a7b37df92d9dab2e4a567b546c6f0889dff379e8
YashBodhe2004 <yashbodhe2004@gmail.com>
2024-01-22 12:00:43 +0530

    added app.js and some css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import questions from "./questions";
-import Result from "./components/Result";
-import QuestionBox from "./components/QuestionBox";
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import questionsData from './questions';
+import Result from './components/Result';
+import QuestionBoxModified from './components/QuestionBox';
 
+function AppModified() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [currentTheme, setCurrentTheme] = useState('Dark');
 
+  useEffect(() => {
+    document.body.style.backgroundColor = isDarkMode ? 'rgb(74, 48, 0)' : 'orange';
+    document.body.style.color = isDarkMode ? 'black' : 'black';
+  }, [isDarkMode]);
 
-function App() {
+  let toggleDarkMode = function () {
+    setIsDarkMode((prev) => !prev);
+    setCurrentTheme((prev) => (prev === 'Dark' ? 'Light' : 'Dark'));
+  };
 
   return (
     <div>
-      
+      <QuestionBoxModified questions={questionsData} totalQuestions={questionsData.length} />
+      <button onClick={toggleDarkMode} className='Ambient appButton'>
+        {currentTheme}
+      </button>
     </div>
   );
 }
 
-export default App;
+export default AppModified;
